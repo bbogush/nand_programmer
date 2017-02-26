6e8b214cf534fe845842c7c4b269b9d2c553c22f
Bogdan Bogush <bogdan.s.bogush@gmail.com>
2017-02-26 17:51:32 +0200

    Added USB library

--- Makefile
+++ Makefile
@@ -8,7 +8,8 @@ PROG=$(OBJ_DIR)$(PROG_NAME)
 SPL_PATH=libs/spl/
 SPL_DEVICE_SUPPORT_PATH=$(SPL_PATH)CMSIS/CM3/DeviceSupport/ST/STM32F10x/
 SPL_CORE_SUPPORT=$(SPL_PATH)CMSIS/CM3/CoreSupport/
-SPL_DRIVER=$(SPL_PATH)STM32F10x_StdPeriph_Driver/
+SPL_PERIPH_PATH=$(SPL_PATH)STM32F10x_StdPeriph_Driver/
+SPL_USB_PATH=$(SPL_PATH)STM32_USB-FS-Device_Driver/
 SPL_LIB=stm32f10x
 SPL_CONFIG_FILE=$(SPL_PATH)stm32f10x_conf.h
 SPL_FLAGS=-DSTM32F10X_HD
@@ -23,7 +24,9 @@ SIZE=$(TOOLCHAIN)size
 INCLUDES=-include$(SPL_CONFIG_FILE)
 INCLUDES+=-I$(SPL_CORE_SUPPORT)
 INCLUDES+=-I$(SPL_DEVICE_SUPPORT_PATH)
-INCLUDES+=-I$(SPL_DRIVER)inc
+INCLUDES+=-I$(SPL_PATH)
+INCLUDES+=-I$(SPL_PERIPH_PATH)inc
+INCLUDES+=-I$(SPL_USB_PATH)inc
 INCLUDES+=-I$(SRC_DIR)
 
 CFLAGS=-g -Wall -Werror
@@ -39,7 +42,9 @@ vpath %.s $(SRC_DIR)
 
 STARTUP=startup_stm32f10x_hd.s
 
-SRCS=main.c system_stm32f10x.c syscalls.c stm3210e_eval_fsmc_nand.c
+SRCS=main.c system_stm32f10x.c syscalls.c stm3210e_eval_fsmc_nand.c \
+  hw_config.c stm32_it.c usb_prop.c usb_desc.c usb_istr.c usb_pwr.c usb_endp.c
+
 OBJS=$(addprefix $(OBJ_DIR),$(SRCS:.c=.o)) \
   $(addprefix $(OBJ_DIR),$(STARTUP:.s=.o))
 DEPS=$(OBJS:%.o=%.d)
--- src/main.c
+++ src/main.c
@@ -1,6 +1,9 @@
 #include "stm3210e_eval_fsmc_nand.h"
 /* SPL */
 #include <stm32f10x.h>
+#include <usb_lib.h>
+#include <usb_pwr.h>
+#include "hw_config.h"
 
 #define PAGE_NUM      2
 #define BUFFER_SIZE   (PAGE_NUM * NAND_PAGE_SIZE)
@@ -10,6 +13,13 @@ NAND_ADDRESS WriteReadAddr;
 uint8_t TxBuffer[BUFFER_SIZE], RxBuffer[BUFFER_SIZE];
 uint32_t PageNumber = 2, WriteReadStatus = 0, status= 0;
 uint32_t j = 0;
+
+uint32_t packet_sent=1;
+uint32_t packet_receive=1;
+extern __IO uint8_t Receive_Buffer[64];
+extern __IO  uint32_t Receive_length ;
+extern __IO  uint32_t length ;
+uint8_t Send_Buffer[64];
 
 void Fill_Buffer(uint8_t *pBuffer, uint16_t BufferLenght, uint32_t Offset)
 {
@@ -22,8 +32,50 @@ void Fill_Buffer(uint8_t *pBuffer, uint16_t BufferLenght, uint32_t Offset)
     }
 }
 
+static void usb_periph_init()
+{
+    GPIO_InitTypeDef GPIO_InitStructure;
+    EXTI_InitTypeDef EXTI_InitStructure;
+    NVIC_InitTypeDef NVIC_InitStructure;
+
+    /*Set PA11,12 as IN - USB_DM,DP*/ 
+    RCC_AHBPeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_11 | GPIO_Pin_12;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    /* Configure the EXTI line 18 connected internally to the USB IP */
+    EXTI_ClearITPendingBit(EXTI_Line18);
+    EXTI_InitStructure.EXTI_Line = EXTI_Line18;
+    EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Rising;
+    EXTI_InitStructure.EXTI_LineCmd = ENABLE;
+    EXTI_Init(&EXTI_InitStructure);
+
+    RCC_USBCLKConfig(RCC_USBCLKSource_PLLCLK_1Div5);
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_USB, ENABLE);
+
+    /* 2 bit for pre-emption priority, 2 bits for subpriority */
+    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+
+    /* Enable the USB interrupt */
+    NVIC_InitStructure.NVIC_IRQChannel = USB_LP_CAN1_RX0_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 2;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+  
+    /* Enable the USB Wake-up interrupt */
+    NVIC_InitStructure.NVIC_IRQChannel = USBWakeUp_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
+    NVIC_Init(&NVIC_InitStructure); 
+}
+
 int main()
 {
+    usb_periph_init();
+    USB_Init();
+
     RCC_AHBPeriphClockCmd(RCC_AHBPeriph_FSMC, ENABLE);
     NAND_Init();
     NAND_ReadID(&NAND_ID);
@@ -54,7 +106,23 @@ int main()
         }
     }
 
-    while(1);
+    while (1)
+    {
+        if (bDeviceState == CONFIGURED)
+        {
+            CDC_Receive_DATA();
+            /*Check to see if we have data yet */
+            if (Receive_length  != 0)
+            {
+                if (packet_sent == 1)
+		{
+                    CDC_Send_DATA ((unsigned char*)Receive_Buffer,
+                        Receive_length);
+                }
+                Receive_length = 0;
+            }
+        }
+    }
 
     return 0;
 }
