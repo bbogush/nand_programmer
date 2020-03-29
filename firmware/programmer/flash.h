9de264061505d439eb3099efda3817d2db9b6ba0
Bogdan Bogush <bogdan.s.bogush@gmail.com>
2020-03-29 18:29:31 +0300

    Fixed double inclusion protection in header

--- firmware/programmer/flash.h
+++ firmware/programmer/flash.h
@@ -3,6 +3,11 @@
  *  it under the terms of the GNU General Public License version 3.
  */
 
+#ifndef _FLASH_H_
+#define _FLASH_H_
+
 int flash_page_erase(uint32_t page_addr);
 int flash_write(uint32_t addr, uint8_t *data, uint32_t data_len);
 int flash_read(uint32_t addr, uint8_t *data, uint32_t data_len);
+
+#endif
