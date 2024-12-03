254d774299087c2ee566d40a122285975a77ef75
liqili <liqili2005@gmail.com>
2024-12-03 18:27:27 +1300

    Add cmaker support for QT compilation on Macos Set default baudrate to 115200 to avoid issue with MacOS launch

--- qt/programmer.cpp
+++ qt/programmer.cpp
@@ -12,7 +12,7 @@
   #define SERIAL_PORT_SPEED 4000000
 #else
   #define USB_DEV_NAME "COM1"
-  #define SERIAL_PORT_SPEED 4000000
+  #define SERIAL_PORT_SPEED 115200
 #endif
 
 #define READ_TIMEOUT_MS 100
