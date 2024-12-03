3ff6b32c3f62e907a822aae4081f92fc825844b8
liqili <liqili2005@gmail.com>
2024-12-03 22:16:08 +1300

    build macos bundle

--- qt/main.cpp
+++ qt/main.cpp
@@ -8,10 +8,10 @@
 
 int main(int argc, char *argv[])
 {
-    if (QSysInfo::productType() == "osx") {
-        // Force non-native menu bar on macOS
-        QCoreApplication::setAttribute(Qt::AA_DontUseNativeMenuBar, true);
-    }
+//    if (QSysInfo::productType() == "osx") {
+//        // Force non-native menu bar on macOS
+//        QCoreApplication::setAttribute(Qt::AA_DontUseNativeMenuBar, true);
+//    }
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
